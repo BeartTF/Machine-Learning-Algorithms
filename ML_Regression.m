efb9576cacec2fd4e5c2aa9a6059b2cc75e67735
BeartTF <117646542+BeartTF@users.noreply.github.com>
2023-07-20 16:35:57 +0300

    Update ML_Regression.m

--- ML_Regression.m
+++ ML_Regression.m
@@ -7,9 +7,11 @@ r = randn(5,1)*10;
 Y = 2.*data(:,1) + 4.*data(:,2) + 4.*data(:,3) + 8.*data(:,4) + r;
 learningRate = 0.0001;
 itnumber = 100000;
+lambda = 1;
 
 [B] = flinreg(X,Y,learningRate,itnumber)
-
+[B] = flinregL1(X,Y,learningRate,itnumber,lambda)
+[B] = flinregL2(X,Y,learningRate,itnumber,lambda)
 
 function [B] = flinreg(X,Y,learningRate,itnumber)
 [row,~] = size(X);
@@ -17,11 +19,53 @@ data = [ones(row,1) X];
 B = (data.'*data)^-1 * data.'*Y;
 
 for i = 1:itnumber
-%     JB = sum((Y-data*B).^2)/row;
+%     JB = sum((data*B-Y).^2)/row;
 %     if 1 == sum(i == [2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000])
 %         abs(dB) >= abs(((Y-data*B).'*data/row).')
 %     end
     dB = ((data*B-Y).'*data/row).';
+    B = B - learningRate * dB;
+%     if 1 == sum(i == [1,2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000])
+%         dB
+%         B
+%     end
+end
+
+% B
+end
+
+function [B] = flinregL1(X,Y,learningRate,itnumber,lambda)
+[row,col] = size(X);
+data = [ones(row,1) X];
+B = (data.'*data + lambda*eye(col+1))^-1 * data.'*Y;
+
+for i = 1:itnumber
+%     JB = sum((data*B-Y).^2 + lambda*abs(B))/row;
+%     if 1 == sum(i == [2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000])
+%         abs(dB) >= abs(((Y-data*B).'*data/row).')
+%     end
+    dB = (((data*B-Y).'*data + lambda*B.'/abs(B.'))/row).';
+    B = B - learningRate * dB;
+%     if 1 == sum(i == [1,2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000])
+%         dB
+%         B
+%     end
+end
+
+% B
+end
+
+function [B] = flinregL2(X,Y,learningRate,itnumber,lambda)
+[row,col] = size(X);
+data = [ones(row,1) X];
+B = (data.'*data + lambda*eye(col+1))^-1 * data.'*Y;
+
+for i = 1:itnumber
+%     JB = sum((data*B-Y).^2 + lambda*B.^2)/row;
+%     if 1 == sum(i == [2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000])
+%         abs(dB) >= abs(((Y-data*B).'*data/row).')
+%     end
+    dB = (((data*B-Y).'*data + lambda*B.')/row).';
     B = B - learningRate * dB;
 %     if 1 == sum(i == [1,2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000])
 %         dB
