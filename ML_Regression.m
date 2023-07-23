38a1724c05f49f87d78481535625d78ddf8e3508
BeartTF <117646542+BeartTF@users.noreply.github.com>
2023-07-23 21:55:53 +0300

    Update ML_Regression.m

--- ML_Regression.m
+++ ML_Regression.m
@@ -1,19 +1,26 @@
 Betaski = 151;
 
-X = [90 60 90;90 90 30;60 60 60;60 60 90;30 30 30];
-[row,~] = size(X);
-data = [ones(row,1) X];
-r = randn(5,1)*10
-Y = 2.*data(:,1) + 4.*data(:,2) + 4.*data(:,3) + 8.*data(:,4) + r;
-learningRate = 0.0001;
-itnumber = 100000;
-lambda = 1;
-Xr = 90*rand(5,3);
+% X = [90 60 90;90 90 30;60 60 60;60 60 90;30 30 30];
+% [row,~] = size(X);
+% data = [ones(row,1) X];
+% r = randn(5,1)*10
+% Y = 2.*data(:,1) + 4.*data(:,2) + 4.*data(:,3) + 8.*data(:,4) + r;
+% learningRate = 0.0001;
+% itnumber = 100000;
+% lambda = 1;
+% Xr = 90*rand(5,3);
+% 
+% [B] = flinreg(X,Y,learningRate,itnumber)
+% [B] = flinregL1(X,Y,learningRate,itnumber,lambda)
+% [B] = flinregL2(X,Y,learningRate,itnumber,lambda)
+% [Y_pred] = tlinreg(Xr,B)
 
-[B] = flinreg(X,Y,learningRate,itnumber)
-[B] = flinregL1(X,Y,learningRate,itnumber,lambda)
-[B] = flinregL2(X,Y,learningRate,itnumber,lambda)
-[Y_pred] = tlinreg(Xr,B)
+rdata = randi([1 10],20,2).*[5 8];
+rdata = [rdata rdata*[8;5]+randi([1 10],20,1)];
+k = 3;
+Xr = randi([1 10],1,2).*[5 8];
+
+[Y_pred,indx] = knnreg(rdata(:,1:end-1),rdata(:,end),k,Xr)
 
 function [B] = flinreg(X,Y,learningRate,itnumber)
 [row,~] = size(X);
@@ -81,4 +88,14 @@ end
 function [Y_pred] = tlinreg(Xr,B)
 [row,~] = size(Xr);
 Y_pred = [ones(row,1) Xr] * B;
+end
+
+function [Y_pred,indx] = knnreg(X,Y,k,Xr)
+% X = rdata(:,1:end-1);
+% Y = rdata(:,end);
+% k = 3;
+[~,col]= size(X(:,:));
+dist = sqrt((X - Xr).^2 * ones(col,1));
+[~,indx] = mink(dist,k);
+Y_pred = Y(indx).' * ones(k,1) / k;
 end
